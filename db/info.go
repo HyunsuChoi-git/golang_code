7800f002c31e87b8ead23f4a360308342b81044a
HyunsuChoi-git <choihs1054@gmail.com>
2024-02-19 19:51:33 +0900

    Mon Feb 19 19:51:31 KST 2024 19:51:31

--- db/info.go
+++ db/info.go
@@ -4,9 +4,9 @@ var (
 	DbTyps       = "postgres"
 	Host         = "10.10.10.135"
 	Port         = 5432
-	DbName       = "vista_mon_hschoi"
-	UserName     = "tuba_om_user"
-	UserPassword = "Okestro2018!"
+	DbName       = ""
+	UserName     = ""
+	UserPassword = ""
 	Encoding     = "utf8"
 	ConnTimeout  = 5
 )
